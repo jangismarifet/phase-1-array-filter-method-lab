8027261674288461d153de6b845a59c8ade36b6c
jangismarifet <jangismarifet@gmail.com>
2022-04-24 21:58:18 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(arr,word) {
+    const res = arr.filter(item => item.toLowerCase() === word.toLowerCase());
+    return res;
+}
+function fuzzyMatch(arr,word) {
+    const res = arr.filter(item => item.startsWith(word));
+    return res;
+}
+function matchName(arr,word) {
+    const res = arr.filter(item => item.name === word);
+    return res;
+}
